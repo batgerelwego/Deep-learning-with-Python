f50fd4c8c95bb59e4064261c4ee255700b76e5b9
batgerelt@must.edu.mn <batgerelt@must.edu.mn>
2019-07-29 11:46:47 +0900

    mnist = tf.keras.datasets.mnist

--- Tensorflow_keras.py
+++ Tensorflow_keras.py
@@ -1,3 +1,14 @@
 import tensorflow.keras as keras
-import tensorflow as tf 
-print(tf._version_)
+import tensorflow as tf
+import matplotlib.pyplot as plt
+print(tf.__version__)
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+print(x_train[0])
+
+print(y_train[0])
+plt.imshow(x_train[0],cmap=plt.cm.binary)
+plt.show()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
